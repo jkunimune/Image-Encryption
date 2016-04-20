b9c79f087218d119e80cca7dffa95980a28eed48
Justin Kunimune <justinkunimune@gmail.com>
2016-04-20 10:04:01 -0400

    Minor Change

--- image_to_matrix.m
+++ image_to_matrix.m
@@ -1,8 +1,8 @@
-function output = image_to_matrix(filename)
+function imageOutput = image_to_matrix(filename)
 
 img = imread(filename); % load the image
 
-output = (img(:,:,1)*.3 + img(:,:,2)*.59 + img(:,:,3)*.11);    % convert to grayscale matrix
+imageOutput = (img(:,:,1)*.3 + img(:,:,2)*.59 + img(:,:,3)*.11);    % convert to grayscale matrix
 
 imshow(output);         % show the image
 
