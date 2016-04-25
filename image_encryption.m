52924004d5ff575e32019a173a8c843886fba5ce
Justin Kunimune <justinkunimune@gmail.com>
2016-04-25 10:26:21 -0400

    The Whiteness of the Whale
    I encoded Moby Dick. You can now get it in .txt, .png, and .mat form!

--- image_encryption.m
+++ image_encryption.m
@@ -3,10 +3,12 @@
 mat = image_to_matrix(input('Filename of encryption image: ','s'));
 
 code = encodeTextFixed(mat);
+img = uint8((code-min(min(code)))*255/(max(max(code))-min(min(code))));
 figure;
-imshow(uint8((code-min(min(code)))*255/(max(max(code))-min(min(code)))))
-
-%mat = do_SVD(mat, min(size(mat))*.9);
+imshow(img);
+imwrite(img, 'mobydick.png');
+disp(code)
+%mat = do_SVD(mat, min(size(mat))*.5);
 
 decoded = decodeTextFixed(code, mat);
 disp(decoded)
--- image_to_matrix.m
+++ image_to_matrix.m
@@ -2,6 +2,10 @@ function imageOutput = image_to_matrix(filename)
 
 img = imread(filename); % load the image
 
-imageOutput = (img(:,:,1)*.3 + img(:,:,2)*.59 + img(:,:,3)*.11);    % convert to grayscale matrix
+if length(img(1,1,:)) > 1
+    imageOutput = (img(:,:,1)*.3 + img(:,:,2)*.59 + img(:,:,3)*.11);    % convert to grayscale matrix
+else
+    imageOutput = img;
+end
 
 end
